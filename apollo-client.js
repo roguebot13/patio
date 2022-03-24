2a6f2fd016577c5f1a54b5951a86b78e7b8cf4c6
Snehil <gitea@fake.local>
2022-03-24 20:35:48 +0530

    Pagination enabled using gql variables

--- apollo-client.js
+++ apollo-client.js
@@ -2,7 +2,28 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
   uri: 'https://api-mumbai.lens.dev',
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          explorePublications: {
+            // Don't cache separate results based on
+            // any of this field's arguments.
+            keyArgs: ['sortCriteria'],
+
+            // Concatenate the incoming list items with
+            // the existing list items.
+            merge(existing = { items: [] }, incoming) {
+              return {
+                ...incoming,
+                items: [...existing.items, ...incoming.items],
+              }
+            },
+          },
+        },
+      },
+    },
+  }),
 })
 
 export default client
--- components/ExploreFeed.js
+++ components/ExploreFeed.js
@@ -1,37 +1,31 @@
 import { useQuery } from '@apollo/client'
 import dayjs from 'dayjs'
+import { useEffect } from 'react'
 import {
-  explorePublicationsQuery,
+  EXPLORE_PUBLICATIONS_QUERY,
   ExlporePublicationsSortCriteria,
 } from '../gql/explorePublications'
 
 export default function ExploreFeed({
   sortCriteria = ExlporePublicationsSortCriteria.TOP_COMMENTED,
 }) {
-  const { data, loading, error } = useQuery(
-    explorePublicationsQuery({ sortCriteria }),
+  const { data, loading, error, fetchMore, refetch } = useQuery(
+    EXPLORE_PUBLICATIONS_QUERY,
+    {
+      variables: {
+        sortCriteria: sortCriteria,
+        cursor: '{}',
+      },
+    },
   )
+
+  useEffect(refetch, [sortCriteria])
 
   if (loading) {
     return (
-      <h2>
-        <a href="#loading" aria-hidden="true" class="aal_anchor" id="loading">
-          <svg
-            aria-hidden="true"
-            class="aal_svg"
-            height="16"
-            version="1.1"
-            viewBox="0 0 16 16"
-            width="16"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
-            ></path>
-          </svg>
-        </a>
-        Loading...
-      </h2>
+      <div className="text-center">
+        <a className="btn btn-ghost loading">Loading...</a>
+      </div>
     )
   }
 
@@ -76,6 +70,21 @@ export default function ExploreFeed({
           </div>
         </div>
       ))}
+      <div className="text-center p-4">
+        <button
+          className="btn btn-primary btn-wide"
+          onClick={() => {
+            fetchMore({
+              variables: {
+                sortCriteria: sortCriteria,
+                cursor: data.explorePublications.pageInfo.next,
+              },
+            })
+          }}
+        >
+          Load More
+        </button>
+      </div>
     </>
   )
 }
